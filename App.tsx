7d651eff2f1294612561eab792b8681de86889f9
Muhammad Naseem <alinaseem366@gmail.com>
2024-08-20 20:20:35 +0500

    Stripe successfully integrated

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import {Text} from 'react-native'
 import { MyStack } from './src/Navigations/StackNav.';
+import { StripeProvider } from '@stripe/stripe-react-native';
 const App = ()=> {
   return(
+    <StripeProvider
+    publishableKey="pk_test_51PppyR01EfaDZ1wW8QKHUjvDXJeDcBWfxQPlQflwggDDhCIA0EBU71Ho0PVDqSyQ362W6H2dmhhnNZNqDRxmVIhj00wn4aL180"
+>
    <MyStack/>
-      // <Text>Hello World</Text>
-  
+      </StripeProvider>
   )
 }
 export default App;
